20ab3715c01df38c429da29f61f9650f0fbbc23c
Thibaut B <t.batal@le-campus-numerique.fr>
2021-01-29 11:36:15 +0100

    Languages

--- src/dist/js/util/text/Languages.js
+++ src/dist/js/util/text/Languages.js
@@ -1,24 +1,36 @@
 "use strict";
 
 class Languages {
-  static singleton = null;
+  static codes_languages = require("./language_codes");
+  static codes_countries = require("./country_codes");
 
-  static codes = require("./language_codes");
+  //
+  // Init by constructor
+  // reverse of codes_languages
+  static languages_codes = {};
+  // reverse of codes_countries
+  static countries_codes = {};
 
-  // init by constructor
-  static codes_list = [];
+  static get singleton() {
+    if (Languages.singleton) {
+      return Languages.singleton;
+    }
 
-  static create_dom_select(select_id, select_name) {
+    Languages.singleton = new Languages_();
+    return Languages.singleton;
+  }
+
+  static get_dom_select(select_id, select_name) {
     let select = $(
       "<select id='" + select_id + "' name='" + select_name + "'></select>"
     );
 
-    for (const lg_code in Languages.codes) {
+    for (const lg_code in Languages.codes_languages) {
       const opt = $(
         "<option value='" +
           lg_code +
           "'>" +
-          Languages.codes[lg_code] +
+          Languages.codes_languages[lg_code] +
           "</option>"
       );
       select.append(opt);
@@ -27,20 +39,52 @@ class Languages {
     return select;
   }
 
-  static get() {
-    if (Languages.singleton) {
-      return Languages.singleton;
-    }
+  //
+  // === GETTERS ===
+  static get_country(country_code) {
+    return Languages.codes_countries[country_code];
+  }
 
-    Languages.singleton = new Languages();
-    return Languages.singleton;
+  static get_country_code(country) {
+    return Languages.countries_codes[country];
+  }
+
+  static get_language(lang_code) {
+    return Languages.codes_languages[lang_code];
+  }
+
+  static get_language_code(language) {
+    return Languages.languages_codes[language];
+  }
+
+  //
+  // === IS ... ===
+  static is_country(country) {
+    return Languages.countries_codes[country] != null;
+  }
+
+  static is_country_code(country_code) {
+    return Languages.codes_countries[country_code] != null;
+  }
+
+  static is_language(language) {
+    return Languages.languages_codes[language] != null;
+  }
+
+  static is_language_code(lang_code) {
+    Obj_Errors.log = "Languages#is_language_code";
+    return Languages.codes_languages[lang_code] != null;
   }
 
   constructor() {
-    for (let code in Languages.codes) {
-      codes_list.push(code);
+    for (const code in Languages.codes_languages) {
+      Languages.languages_codes[Languages.codes_languages[code]] = code;
+    }
+
+    for (const code in Languages.codes_countries) {
+      Languages.countries_codes[Languages.codes_countries[code]] = code;
     }
   }
 }
 
-module.exports = Languages;
+util.obj.Obj.export(module, Languages);
