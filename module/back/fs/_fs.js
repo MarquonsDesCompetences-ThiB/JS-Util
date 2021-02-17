07dcaed239d9fb89b793267065fe3f32e6351d25
Thibaut B <t.batal@le-campus-numerique.fr>
2021-02-17 18:14:41 +0100

    fix export fs.tree

--- module/back/fs/_fs.js
+++ module/back/fs/_fs.js
@@ -1,10 +1,3 @@
 export * from "./fs.js";
-import { Directory_Tree } from "./tree/Directory_Tree.js";
-import { Directory_Tree_Root } from "./tree/Directory_Tree_Root.js";
-import { Directory_Tree_Slave } from "./tree/slave/Directory_Tree_Slave.js";
-export const tree = {
-    Master_Root: Directory_Tree_Root,
-    Master_Node: Directory_Tree,
-    Slave: Directory_Tree_Slave,
-};
+export * as tree from "./tree/_tree.js";
 //# sourceMappingURL=_fs.js.map
---
+++ module/back/fs/tree/_tree.js
@@ -0,0 +1,4 @@
+export { Directory_Tree as Master_Node } from "./Directory_Tree.js";
+export { Directory_Tree_Root as Master_Root } from "./Directory_Tree_Root.js";
+export { Directory_Tree_Slave as Slave } from "./slave/Directory_Tree_Slave.js";
+//# sourceMappingURL=_tree.js.map
